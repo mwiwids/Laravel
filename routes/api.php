2c89fb219766da449da274c1aa690006e2bb2ff9
mwiwids <m.wiwid.s@gmail.com>
2020-11-29 01:34:39 +0700

    step 2, RESTful API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+/**
+ * route resource post
+ */
+Route::resource('/post', PostController::class);
